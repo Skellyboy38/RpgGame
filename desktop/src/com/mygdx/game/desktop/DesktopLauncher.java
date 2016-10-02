a4d5fcb6d299a7f2900c7a63380283164b91990b
Robert McAuley <Robert McAuley>
2016-10-02 13:44:58 -0400

    modified the game dimensions to match screen dimensions

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,8 @@
 package com.mygdx.game.desktop;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.RpgGame;
@@ -7,6 +10,13 @@ import com.mygdx.game.RpgGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "SUPER SMASH";	//The title that appears when the game opens
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double width = screenSize.getWidth();	//The width of the computer screen
+		double height = screenSize.getHeight();	//Height of the computer screen
+		config.width = (int)width;		//Sets the width and height of the game 
+		config.height = (int)height;	//This is the height of the screen
+		//config.fullscreen = true;		//Starts the game in full screen mode
 		new LwjglApplication(new RpgGame(), config);
 	}
 }
